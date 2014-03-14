638d4013222e4cd09d51d67513e3d2316173c72d
Massimiliano Fanciulli <fanciulli@gmail.com>
2014-03-14 14:18:19 +0100

    Added heat refresh when clicking button

--- ui/js/app-controller.js
+++ ui/js/app-controller.js
@@ -1,12 +1,19 @@
-var appModule=angular.module('AppControllers', []);
-appModule.controller('AppController', function($scope,$routeParams,$http) {
-    $scope.hbeat_today=0;
-   
-    $http.get('/api/1/hbeat/'+$routeParams.appId+'/today',{headers:{ 'Authorization' : '0GqtOtD4SSrJ1MUXj0ffkXLTt0a9ujYCZF6hMLtqfX7W0LA2SVQ3jPouAwgGnSa0'}})
-    .success(function(data) {
-      $scope.hbeat_today=data['beat_count'];
-    }).error(function(data, status, headers, config) {
-      $scope.hbeat_today=0;
-      alert("Error "+status+" "+data);
-    });
+var appModule = angular.module('AppControllers', []);
+appModule.controller('AppController', function($scope, $routeParams, $http) {
+    $scope.hbeat_today = 0;
+
+    $scope.refresh_hbeat = function()
+    {
+        $http.get('/api/1/hbeat/' + $routeParams.appId + '/today', {headers: {'Authorization': '0GqtOtD4SSrJ1MUXj0ffkXLTt0a9ujYCZF6hMLtqfX7W0LA2SVQ3jPouAwgGnSa0'}})
+                .success(function(data) {
+                    $scope.hbeat_today = data['beat_count'];
+                }).error(function(data, status, headers, config) {
+            $scope.hbeat_today = 0;
+            alert("Error " + status + " " + data);
+        });
+    }
+    
+    $scope.refresh_hbeat();
+    
+    
 }); 
