49629589823541b5c3856039c2dcdcef25f15f15
Massimiliano Fanciulli <fanciulli@gmail.com>
2014-04-08 14:13:41 +0200

    Added auto refresh

--- ui/js/app-controller.js
+++ ui/js/app-controller.js
@@ -1,5 +1,5 @@
 var appModule = angular.module('AppControllers', []);
-appModule.controller('AppController', function($scope, $routeParams, $http) {
+appModule.controller('AppController', function($scope, $routeParams, $http, $interval) {
     $scope.hbeat_today = 0;
     $scope.app_id=$routeParams.appId;
     
@@ -26,7 +26,8 @@ appModule.controller('AppController', function($scope, $routeParams, $http) {
     
     $scope.refresh_hbeat = function()
     {
-        $http.get('/api/1/hbeat/' + $routeParams.appId + '/today', {headers: {'Authorization': '0GqtOtD4SSrJ1MUXj0ffkXLTt0a9ujYCZF6hMLtqfX7W0LA2SVQ3jPouAwgGnSa0'}})
+        $http.get('/api/1/hbeat/' + $routeParams.appId + '/today', {headers: {'Content-Type': 'application/json'}})
+                                                                    //{'Authorization': '0GqtOtD4SSrJ1MUXj0ffkXLTt0a9ujYCZF6hMLtqfX7W0LA2SVQ3jPouAwgGnSa0'}})
                 .success(function(data) {
                     $scope.hbeat_today = data['beat_count'];
                 }).error(function(data, status, headers, config) {
@@ -61,6 +62,7 @@ appModule.controller('AppController', function($scope, $routeParams, $http) {
                     }
                     $scope.hbeat_graph_data = graph_data;
                     $scope.refresh_graph();
+        
                 }).error(function(data, status, headers, config) {
             $scope.hbeat_graph_data = 0;
         });
@@ -147,15 +149,25 @@ appModule.controller('AppController', function($scope, $routeParams, $http) {
 
 
     $scope.refresh_graph=function(){
-    var ctx = document.getElementById("myChart").getContext("2d");
-    ctx.canvas.width = window.innerWidth - 400;
-    ctx.canvas.height = 200;
-    var myNewChart = new Chart(ctx).Line($scope.hbeat_graph_data);
-}
+        var ctx = document.getElementById("myChart").getContext("2d");
+        ctx.canvas.width = window.innerWidth - 400;
+        ctx.canvas.height = 200;
+        var myNewChart = new Chart(ctx).Line($scope.hbeat_graph_data);
+    }
     
-    $scope.refresh_hbeat();
-    $scope.refresh_hbeat_graph();
-    $scope.refresh_graph();
-    $scope.get_feedbacks();
-    $scope.get_exceptions();
+    $scope.refresh_page_data=function()
+    {
+        $scope.refresh_hbeat();
+        $scope.refresh_hbeat_graph();
+        $scope.get_feedbacks();
+        $scope.get_exceptions();
+    }
+    
+    var stopTime = $interval($scope.refresh_page_data, 60000);
+ 
+    $scope.$on("$destroy", function(){
+       $interval.cancel(stopTime);
+    });    
+    
+    $scope.refresh_page_data();
 }); 
