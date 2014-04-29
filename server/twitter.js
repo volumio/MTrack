f234db5c03a929e92a5eed69cf82302af9107fe5
Massimiliano Fanciulli <fanciulli@gmail.com>
2014-04-29 14:13:39 +0200

    Fixed Twitter account

--- server/twitter.js
+++ server/twitter.js
@@ -8,15 +8,17 @@ var config = require('twit');
 var Twit = require('twit')
 
 var T = new Twit({
-    consumer_key:         '2siVfQEdmEXzhxei37Kc1jlf7'
-  , consumer_secret:      'RNocyGrGRvFNLGMfDhC0Tn8AtASKZlhmnwnn36XUQtetfN1EZb'
-  , access_token:         '1635817243-zzOdR909fhZKCXShfCvqL0BFPK7MZApCOma5qJc'
-  , access_token_secret:  'kP3YMxmhEDzcnkowBGEXvXRaSpCyZesQo8jXs9FUCpjlJ'
+    consumer_key:         'WNK6VXhDc49KDMiO1xoYypBrK'
+  , consumer_secret:      '8Zy8MrWmSFhJQbAaNQ2R77UcQoC10DmhaQHvbHdZUqBnDqEgKp'
+  , access_token:         '2510517519-qolfRkCg7j1FbWPc3CsB8UGBfcwS6GWuInihAZu'
+  , access_token_secret:  '0ceoaqngcIVYTiB5LUSFc1vnrMHdmwdPDtkUufrEqtIVZ'
 })
 
 function notify_user_for_feedback(user)
 {
     T.post('direct_messages/new', { user:user ,text: 'Hi this is MTrack! You received a feedback'}, function(err, reply) {
+        if(err!=null)
+            console.log(err);
    
 })
 }
@@ -24,6 +26,8 @@ function notify_user_for_feedback(user)
 function notify_user_for_exception(user)
 {
     T.post('direct_messages/new', { user:user ,text: 'Hi this is MTrack! An exception has been catched!'}, function(err, reply) {
+    if(err!=null)
+            console.log(err);
    
 })
 }
